eb2d3d383516526e037395aeaabadf86c2e0b2f4
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-30 16:52:29 +0800

    office

---
+++ 04office/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="openWord()">打开 Word</button>
+    <div id="word-container"></div>
+  </div>
+</template>
+
+<script>
+import { exportHtmlToDocx } from 'editor-to-word';
+
+export default {
+  data () {
+    return {
+      html: '<p>hello <span style="color: #999">world</span></p>'
+    }
+
+  },
+  methods: {
+    openWord() {
+      exportHtmlToDocx(this.html, 'testFileName');
+    }
+  }
+}
+</script>
