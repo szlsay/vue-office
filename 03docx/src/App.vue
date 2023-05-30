af11c40bb835e5d244c6fb6ad08b80fc7c9652bd
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-05-30 15:35:31 +0800

    docx

---
+++ 03docx/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="file" @change="preview" ref="file" />
+    <div class="my-component" ref="preview"></div>
+  </div>
+</template>
+<script>
+const docx = require("docx-preview");
+window.JSZip = require("jszip");
+export default {
+  methods: {
+    preview() {
+      docx.renderAsync(this.$refs.file.files[0], this.$refs.preview); // 渲染到页面预览
+    },
+  },
+};
+</script>
+<style>
+.my-component {
+  width: 100%;
+  height: 90vh;
+  border: 1px solid #000;
+}
+</style>
