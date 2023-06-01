e60b657c7a03f7c2961506bf7654a8dc1ef882e7
小沈曰 <13865738+szlsay@users.noreply.github.com>
2023-06-01 13:57:42 +0800

    drag

--- 06drag/src/App.vue
+++ 06drag/src/App.vue
@@ -1,38 +1,48 @@
 <template>
   <div>
-    <div>{{ drag ? "拖拽中" : "拖拽停止" }}</div>
     <!--使用draggable组件-->
-    <draggable
-      v-model="myArray"
-      chosenClass="chosen"
-      forceFallback="true"
-      group="people"
-      animation="1000"
-      @start="onStart"
-      @end="onEnd"
-    >
-      <transition-group>
-        <div class="item" v-for="element in myArray" :key="element.id">
-          {{ element.name }}
-        </div>
-      </transition-group>
-    </draggable>
+    <div class="itxst">
+      <div class="col">
+        <div class="title">把下面元素拖拽到B组试试看</div>
+        <draggable
+          v-model="arr1"
+          :group="groupA"
+          animation="300"
+          dragClass="dragClass"
+          ghostClass="ghostClass"
+          chosenClass="chosenClass"
+          @start="onStart"
+          @end="onEnd"
+        >
+          <transition-group :style="style">
+            <div class="item" v-for="item in arr1" :key="item.id">
+              {{ item.name }}
+            </div>
+          </transition-group>
+        </draggable>
+      </div>
+      <div class="col">
+        <div class="title">B组（本组是个空数组）</div>
+        <draggable
+          v-model="arr2"
+          :group="groupB"
+          animation="300"
+          dragClass="dragClass"
+          ghostClass="ghostClass"
+          chosenClass="chosenClass"
+          @start="onStart"
+          @end="onEnd"
+        >
+          <transition-group :style="style">
+            <div class="item" v-for="item in arr2" :key="item.id">
+              {{ item.name }}
+            </div>
+          </transition-group>
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
-<style scoped>
-/*被拖拽对象的样式*/
-.item {
-  padding: 6px;
-  background-color: #fdfdfd;
-  border: solid 1px #eee;
-  margin-bottom: 10px;
-  cursor: move;
-}
-/*选中样式*/
-.chosen {
-  border: solid 2px #3089dc !important;
-}
-</style>
 <script>
 //导入draggable组件
 import draggable from "vuedraggable";
@@ -44,13 +54,26 @@ export default {
   data() {
     return {
       drag: false,
+      groupA: {
+        name: "site",
+        pull: true, //可以拖从
+        put: true, //可以拖出
+      },
+      groupB: {
+        name: "site",
+        pull: true,
+        put: true,
+      },
       //定义要被拖拽对象的数组
-      myArray: [
-        { people: "cn", id: 1, name: "www.itxst.com" },
-        { people: "cn", id: 2, name: "www.baidu.com" },
-        { people: "cn", id: 3, name: "www.taobao.com" },
-        { people: "us", id: 4, name: "www.google.com" },
+      arr1: [
+        { id: 1, name: "www.itxst.com" },
+        { id: 2, name: "www.jd.com" },
+        { id: 3, name: "www.baidu.com" },
+        { id: 3, name: "www.taobao.com" },
       ],
+      arr2: [], //空数组
+      //空数组之在的样式，设置了这个样式才能拖入
+      style: "min-height:120px;display: block;",
     };
   },
   methods: {
@@ -65,3 +88,52 @@ export default {
   },
 };
 </script>
+<style scoped>
+/*定义要拖拽元素的样式*/
+.ghostClass {
+  background-color: blue !important;
+}
+.chosenClass {
+  background-color: red !important;
+  opacity: 1 !important;
+}
+.dragClass {
+  background-color: blueviolet !important;
+  opacity: 1 !important;
+  box-shadow: none !important;
+  outline: none !important;
+  background-image: none !important;
+}
+.itxst {
+  margin: 10px;
+}
+.title {
+  padding: 6px 12px;
+}
+.col {
+  width: 40%;
+  flex: 1;
+  padding: 10px;
+  border: solid 1px #eee;
+  border-radius: 5px;
+  float: left;
+}
+.col + .col {
+  margin-left: 10px;
+}
+
+.item {
+  padding: 6px 12px;
+  margin: 0px 10px 0px 10px;
+  border: solid 1px #eee;
+  background-color: #f1f1f1;
+}
+.item:hover {
+  background-color: #fdfdfd;
+  cursor: move;
+}
+.item + .item {
+  border-top: none;
+  margin-top: 6px;
+}
+</style>
---
+++ 06drag/src/App00.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div>{{ drag ? "拖拽中" : "拖拽停止" }}</div>
+    <!--使用draggable组件-->
+    <draggable
+      v-model="myArray"
+      chosenClass="chosen"
+      forceFallback="true"
+      group="people"
+      animation="1000"
+      @start="onStart"
+      @end="onEnd"
+    >
+      <transition-group>
+        <div class="item" v-for="element in myArray" :key="element.id">
+          {{ element.name }}
+        </div>
+      </transition-group>
+    </draggable>
+  </div>
+</template>
+<style scoped>
+/*被拖拽对象的样式*/
+.item {
+  padding: 6px;
+  background-color: #fdfdfd;
+  border: solid 1px #eee;
+  margin-bottom: 10px;
+  cursor: move;
+}
+/*选中样式*/
+.chosen {
+  border: solid 2px #3089dc !important;
+}
+</style>
+<script>
+//导入draggable组件
+import draggable from "vuedraggable";
+export default {
+  //注册draggable组件
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      drag: false,
+      //定义要被拖拽对象的数组
+      myArray: [
+        { people: "cn", id: 1, name: "www.itxst.com" },
+        { people: "cn", id: 2, name: "www.baidu.com" },
+        { people: "cn", id: 3, name: "www.taobao.com" },
+        { people: "us", id: 4, name: "www.google.com" },
+      ],
+    };
+  },
+  methods: {
+    //开始拖拽事件
+    onStart() {
+      this.drag = true;
+    },
+    //拖拽结束事件
+    onEnd() {
+      this.drag = false;
+    },
+  },
+};
+</script>
